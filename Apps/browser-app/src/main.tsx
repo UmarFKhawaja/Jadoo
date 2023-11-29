12d93c00cb3163e9997aa2dc891c9451eff6065f
Umar F Khawaja <umar.farooq.khawaja@gmail.com>
2023-11-29 10:21:09 +0000

    Fix broken styles.

--- Apps/browser-app/src/main.tsx
+++ Apps/browser-app/src/main.tsx
@@ -5,12 +5,11 @@ import { CookiesProvider } from 'react-cookie';
 import { MantineProvider as FrameworkProvider } from '@mantine/core';
 import { Notifications as NotificationsProvider } from '@mantine/notifications';
 import { SessionProvider } from '@jadoo/ui-module';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
 import { COOKIES_PROVIDER_DEFAULT_SET_OPTIONS } from './constants';
 import { config } from './config';
 import { router } from './router';
-
-import '@mantine/core/styles.css';
-import '@mantine/notifications/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
