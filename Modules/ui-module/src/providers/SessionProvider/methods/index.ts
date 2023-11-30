e61e6c5891adf950f7c78adbbcb92cf70484daf7
Umar Farooq Khawaja <umar.farooq.khawaja@gmail.com>
2023-11-30 18:57:41 +0000

    Add typing info

--- Modules/ui-module/src/providers/SessionProvider/methods/index.ts
+++ Modules/ui-module/src/providers/SessionProvider/methods/index.ts
@@ -2,7 +2,7 @@ import { Config } from '../../../types';
 
 export async function isValidSession(config: Config): Promise<boolean> {
   try {
-    const response = await fetch(`${config.server.url}/auth/check`, {
+    const response: Response = await fetch(`${config.server.url}/auth/check`, {
       method: 'GET',
       credentials: 'include'
     });
